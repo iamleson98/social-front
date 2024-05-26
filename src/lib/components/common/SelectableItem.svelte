29e2124a676ad42aa5d18b7e9f8384952e37f921
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-05-26 19:39:16 +0700

    commit

---
+++ src/lib/components/common/SelectableItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let selected: boolean = false;
+	export let content: string = '';
+
+	const dispatch = createEventDispatcher();
+
+	function selectItem() {
+		dispatch('select');
+	}
+</script>
+
+<div
+	class="border border-gray-300 text-gray-600 px-4 py-1.5 cursor-pointer rounded text-center {selected ? 'bg-red-500 text-white font-normal text-sm' : ''}" 
+	on:click={selectItem}>
+	{content}
+</div>
+
---
+++ src/routes/(app)/products/[slug]/+page.server.ts
@@ -0,0 +1,26 @@
+import type { ProductCountableConnection } from "$lib/gql/graphql";
+import { PRODUCT_LIST_QUERY_STORE } from "$lib/stores/api/product";
+import { HTTPStatusServerError, HTTPStatusSuccess } from "$lib/utils/types";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+    const productsListResult = await PRODUCT_LIST_QUERY_STORE.fetch({
+        event,
+        variables: {
+            channel: "default-channel",
+            first: 1,
+        },
+    });
+
+    if (productsListResult.errors?.length) {
+        return {
+            status: HTTPStatusServerError,
+            error: productsListResult.errors[0].message as string,
+        };
+    }
+
+    return {
+        status: HTTPStatusSuccess,
+        data: productsListResult.data?.products as ProductCountableConnection,
+    };
+};
