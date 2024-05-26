e77e7ece7a16a221b623e0f8242a70ea0bfd6e32
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-05-26 19:50:02 +0700

    commit

--- src/routes/(app)/products/[slug]/+page.server.ts
+++ src/routes/(app)/products/[slug]/+page.server.ts
@@ -2,8 +2,8 @@ import { PRODUCT_LIST_QUERY_STORE } from "$lib/stores/api/product";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
-  PRODUCT_LIST_QUERY_STORE
-  return {
+    PRODUCT_LIST_QUERY_STORE
+    return {
 
-  };
+    };
 };
