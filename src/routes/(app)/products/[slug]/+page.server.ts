66edb22645ebe402b3c86d95f967e297cd1a0f9f
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-05-26 20:15:11 +0700

    commit

--- src/routes/(app)/products/[slug]/+page.server.ts
+++ src/routes/(app)/products/[slug]/+page.server.ts
@@ -1,34 +1,9 @@
-import type { ProductCountableConnection } from "$lib/gql/graphql";
 import { PRODUCT_LIST_QUERY_STORE } from "$lib/stores/api/product";
-import { HTTPStatusServerError, HTTPStatusSuccess } from "$lib/utils/types";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
-    const productsListResult = await PRODUCT_LIST_QUERY_STORE.fetch({
-        event,
-        variables: {
-            channel: "default-channel",
-            first: 1,
-        },
-    });
-
-    if (productsListResult.errors?.length) {
-        return {
-            status: HTTPStatusServerError,
-            error: productsListResult.errors[0].message as string,
-        };
-    }
-
+    PRODUCT_LIST_QUERY_STORE
     return {
-        status: HTTPStatusSuccess,
-        data: productsListResult.data?.products as ProductCountableConnection,
-=======
-import { PRODUCT_LIST_QUERY_STORE } from "$lib/stores/api/product";
-import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async (event) => {
-  PRODUCT_LIST_QUERY_STORE
-  return {
-
-  };
+    };
 };
