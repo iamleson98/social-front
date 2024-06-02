a8fe71621ed8b261e8efd01fe6b6e77af51eeb35
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-06-02 11:55:56 +0700

    fix button size

--- src/routes/(app)/products/[slug]/+page.svelte
+++ src/routes/(app)/products/[slug]/+page.svelte
@@ -147,30 +147,10 @@
 					<p>Add to Cart</p>
 				</button>
 
-				<div class="flex gap-2 w-full mt-20 justify-center">
-					<button class="btn">
-						Button
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-6 w-6"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-							/></svg
-						>
-					</button>
+				<div class="flex flex-row gap-2 w-4/6 ml-auto mr-auto mt-20 justify-center">
 					<ButtonDetailProduct text="365" icon="icon-[system-uicons--heart]" on:click={() => {}} />
-					<ButtonDetailProduct
-						text="365"
-						icon="icon-[system-uicons--bookmark]"
-						on:click={() => {}}
-					/>
-					<div class="relative inline-block">
+					<ButtonDetailProduct text="365" icon="icon-[system-uicons--bookmark]" on:click={() => {}} />
+					<div class="relative w-full sm:w-auto">
 						<ButtonDetailProduct
 							text="365"
 							icon=""
@@ -184,7 +164,7 @@
 							]}
 						/>
 					</div>
-					<div class="relative inline-block">
+					<div class="relative w-full sm:w-auto">
 						<ButtonDetailProduct
 							text=""
 							icon="icon-[system-uicons--menu-horizontal]"
