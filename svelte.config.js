991c27fc200153db61f9a7b3d74c8a9876f0a449
Nothing <leminhson2398@outlook.com>
2024-07-25 07:37:46 +0000

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from "svelte-adapter-bun";
+// import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
