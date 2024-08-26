3107c5b7f14bbc4d5b9d703127b6643f40ad13b8
Son Le <leminhson2398@outlook.com>
2024-08-26 13:03:47 +0700

    Update cart.ts

--- src/lib/stores/app/cart.ts
+++ src/lib/stores/app/cart.ts
@@ -11,56 +11,81 @@ export type CartItemProps = {
 
 /** this util function helps organize cart items before persisting to local storage  */
 export const reorganizeCartItems = (items: CartItemProps[]) => {
-  const productQuantityMap: Record<string, number> = {};
-  const variantQuantityMap: Record<string, number> = {};
+  // const productQuantityMap: Record<string, number> = {};
+  // const variantQuantityMap: Record<string, number> = {};
 
+  const variantItemsMap: Record<string, CartItemProps> = {}; // keys are variant ids
+  const productItemsMap: Record<string, CartItemProps> = {}; // keys are product ids
+  
   for (const item of items) {
     if (item.productName && item.variantId) {
-      if (variantQuantityMap[item.variantId] === undefined) {
-        variantQuantityMap[item.variantId] = item.quantity;
+      if (variantItemsMap[item.variantId] === undefined) {
+        variantItemsMap[item.variantId] = item;
       } else {
-        variantQuantityMap[item.variantId] += item.quantity;
+        variantItemsMap[item.variantId].quantity += item.quantity;
       }
+
       continue;
     }
 
     if (item.productName) {
-      if (productQuantityMap[item.productName] === undefined) {
-        productQuantityMap[item.productName] = item.quantity;
+      if (productItemsMap[item.productName] === undefined) {
+        productItemsMap[item.productName] = item;
       } else {
-        productQuantityMap[item.productName] += item.quantity;
+        productItemsMap[item.productName].quantity += item.quantity;
       }
     }
   }
 
-  const result: CartItemProps[] = [];
-  const variantMeetMap: Record<string, boolean> = {};
-  const productMeetMap: Record<string, boolean> = {};
+  return [...Object.values(variantItemsMap), ...Object.values(productItemsMap)];
 
-  for (const item of items) {
-    if (item.productName && item.variantId) {
-      if (variantMeetMap[item.variantId] !== undefined) continue;
+  // for (const item of items) {
+  //   if (item.productName && item.variantId) {
+  //     if (variantQuantityMap[item.variantId] === undefined) {
+  //       variantQuantityMap[item.variantId] = item.quantity;
+  //     } else {
+  //       variantQuantityMap[item.variantId] += item.quantity;
+  //     }
+  //     continue;
+  //   }
 
-      result.push({
-        ...item,
-        quantity: variantQuantityMap[item.variantId],
-      });
-      variantMeetMap[item.variantId] = true;
-      continue;
-    }
+  //   if (item.productName) {
+  //     if (productQuantityMap[item.productName] === undefined) {
+  //       productQuantityMap[item.productName] = item.quantity;
+  //     } else {
+  //       productQuantityMap[item.productName] += item.quantity;
+  //     }
+  //   }
+  // }
 
-    if (item.productName) {
-      if (productMeetMap[item.productName] !== undefined) continue;
+  // const result: CartItemProps[] = [];
+  // const variantMeetMap: Record<string, boolean> = {};
+  // const productMeetMap: Record<string, boolean> = {};
 
-      result.push({
-        ...item,
-        quantity: productQuantityMap[item.productName],
-      });
-      productMeetMap[item.productName] = true;
-    }
-  }
+  // for (const item of items) {
+  //   if (item.productName && item.variantId) {
+  //     if (variantMeetMap[item.variantId] !== undefined) continue;
 
-  return result;
+  //     result.push({
+  //       ...item,
+  //       quantity: variantQuantityMap[item.variantId],
+  //     });
+  //     variantMeetMap[item.variantId] = true;
+  //     continue;
+  //   }
+
+  //   if (item.productName) {
+  //     if (productMeetMap[item.productName] !== undefined) continue;
+
+  //     result.push({
+  //       ...item,
+  //       quantity: productQuantityMap[item.productName],
+  //     });
+  //     productMeetMap[item.productName] = true;
+  //   }
+  // }
+
+  // return result;
 };
 
 /**
