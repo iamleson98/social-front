9696e021ab801e7f5a53119db894a27bb950f88d
nguyen_phuc <nguyenphuc02042004@gmail.com>
2024-11-15 17:39:37 +0700

    document

---
+++ src/lib/components/pages/document/Content.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { contentData, selectedId } from '../../../../routes/document/data_content';
+	import { derived } from 'svelte/store';
+
+	const content = derived(selectedId, ($selectedId) => contentData[$selectedId]);
+</script>
+
+<div class="content">
+	{@html $content}
+</div>
