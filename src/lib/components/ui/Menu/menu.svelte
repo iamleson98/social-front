69a18ac78463abafc0650591bf59a10bcd5c74d9
Lê Minh <leminh@Les-MacBook-Pro.local>
2024-12-31 22:40:55 +0700

    commit

--- src/lib/components/ui/Menu/menu.svelte
+++ src/lib/components/ui/Menu/menu.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import type { EventHandler } from 'svelte/elements';
+	import { Icon } from '$lib/components/icons';
+	import type { DropDownMenuProps } from './types';
 
-	type Props = {
-		trigger: Snippet<[TriggerIface]>;
-		children: Snippet;
-	};
-
-	let { trigger, children }: Props = $props();
+	let { trigger, selectItems, onSelect }: DropDownMenuProps = $props();
 	let openMenu = $state(false);
 
-	type TriggerIface = {
-		onclick: EventHandler;
-	};
+	// const handleClick = () => {
+	// 	openMenu = !openMenu;
+	// };
 
-	const handleClick = () => {
-		console.log('clicked');
-		openMenu = !openMenu;
+	const handleOpenMenu = (open: boolean) => openMenu = open;
+
+	const handleSelect = (itemIdx: number) => {
+		onSelect(selectItems[itemIdx])
+		// handleClick();
+		handleOpenMenu(false);
 	};
 </script>
 
 <div>
-	{@render trigger({ onclick: handleClick })}
+	{@render trigger({ onclick: () => handleOpenMenu(true) })}
 
 	{#if openMenu}
-		<div class="absolute mt-1">
-			{@render children()}
+		<div class="absolute mt-1 rounded-lg">
+			{#each selectItems as item, idx (idx)}
+				<div class="flex items-center gap-1 py-1.5 px-3"
+					role="button"
+					onclick={() => handleSelect(idx)}
+					tabindex="0"
+					onkeyup={(e) => e.key === 'Enter' && handleSelect(idx)}
+				>
+					{#if item.startIcon}
+						<Icon icon={item.startIcon} />
+					{/if}
+					<span>{item.label}</span>
+				</div>
+			{/each}
 		</div>
 	{/if}
 </div>
