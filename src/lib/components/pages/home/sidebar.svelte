7ac3cdfc092aa049771f8cf3ad2141ad0e093208
Lê Minh <leminhson2398@outlook.com>
2025-04-07 08:35:38 +0700

    commit

--- src/lib/components/pages/home/sidebar.svelte
+++ src/lib/components/pages/home/sidebar.svelte
@@ -2,10 +2,10 @@
 	import { page } from '$app/state';
 	import { CHANNELS, DEFAULT_CHANNEL } from '$lib/utils/channels';
 	import CategoriesAccordion from './sidebar-category.svelte';
-	// import SidebarFilter from './sidebar-filter.svelte';
+	import SidebarFilter from './sidebar-filter.svelte';
 
 	const CHANNEL = CHANNELS.find((chan) => chan.slug === page.params.channel) || DEFAULT_CHANNEL;
 </script>
 
 <CategoriesAccordion />
-<!-- <SidebarFilter currency={CHANNEL.currency} /> -->
+<SidebarFilter currency={CHANNEL.currency} />
