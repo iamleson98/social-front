4bd7c3569097aa9641325f63095620ff2982229d
Son Le <leminhson2398@outlook.com>
2025-05-08 14:52:37 +0700

    commit

--- src/routes/[[channel]]/settings/configs/collections/[id]/+page.svelte
+++ src/routes/[[channel]]/settings/configs/collections/[id]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
   import { page } from '$app/state';
   import { COLLECTION_DETAIL_QUERY } from '$lib/api/admin/collections';
   import { operationStore } from '$lib/api/operation';
@@ -13,24 +12,6 @@
   import { Skeleton, SkeletonContainer } from '$lib/components/ui/Skeleton';
   import type { CollectionInput, Query, QueryCollectionArgs } from '$lib/gql/graphql';
   import { AppRoute } from '$lib/utils';
-=======
-	import { page } from '$app/state';
-	import { COLLECTION_DETAIL_QUERY } from '$lib/api/admin/collections';
-	import { operationStore } from '$lib/api/operation';
-	import ActionBar from '$lib/components/pages/settings/common/action-bar.svelte';
-	import AvailabilityForm from '$lib/components/pages/settings/config/collections/availability-form.svelte';
-	import GeneralInformationForm from '$lib/components/pages/settings/config/collections/general-information-form.svelte';
-	import ProductListForm from '$lib/components/pages/settings/config/collections/product-list-form.svelte';
-	import SeoForm from '$lib/components/pages/settings/config/collections/seo-form.svelte';
-	import { Alert } from '$lib/components/ui/Alert';
-	import { Skeleton, SkeletonContainer } from '$lib/components/ui/Skeleton';
-	import {
-		type CollectionInput,
-		type Query,
-		type QueryCollectionArgs
-	} from '$lib/gql/graphql';
-	import { AppRoute } from '$lib/utils';
->>>>>>> 500f3ab (commit)
 
   const onDeleteClick = () => {};
 
@@ -46,7 +27,6 @@
     }
   });
 
-<<<<<<< HEAD
   $effect(() => {
     if ($collectionDetail.data?.collection) {
       const { name, slug, description, backgroundImage, seoTitle, seoDescription, metadata, privateMetadata } = $collectionDetail.data.collection;
@@ -71,38 +51,6 @@
     collectionUpdateinput.name = newName;
     collectionUpdateinput.slug = newName.toLowerCase().replace(/\s+/g, '-');
   }
-=======
-	$effect(() => {
-		if ($collectionDetail.data?.collection) {
-			const {
-				name,
-				slug,
-				description,
-				backgroundImage,
-				seoTitle,
-				seoDescription,
-				metadata,
-				privateMetadata
-			} = $collectionDetail.data.collection;
-			collectionUpdateinput = {
-				name,
-				slug,
-				description,
-				backgroundImage,
-				seo: {
-					title: seoTitle,
-					description: seoDescription
-				},
-				metadata,
-				privateMetadata,
-				backgroundImageAlt: backgroundImage?.alt
-			};
-		}
-	});
-<<<<<<< HEAD
->>>>>>> 500f3ab (commit)
-=======
->>>>>>> 500f3abcfe76b4361e9ee6a2b085b58799bc12c8
 </script>
 
 {#if $collectionDetail.fetching}
@@ -121,19 +69,10 @@
       <SeoForm slug={collectionUpdateinput.slug as string} seo={collectionUpdateinput.seo as any}/>
     </div>
 
-<<<<<<< HEAD
     <div class="w-3/10">
       <AvailabilityForm />
     </div>
   </div>
-=======
-		<div class="w-3/10">
-			<AvailabilityForm
-				channelListings={$collectionDetail.data.collection?.channelListings || []}
-			/>
-		</div>
-	</div>
->>>>>>> 500f3ab (commit)
 
   <ActionBar
     backButtonUrl={AppRoute.SETTINGS_CONFIGS_COLLECTIONS()}
