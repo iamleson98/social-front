9d55c3e39950364c2c17d92bf460b78d554c7f2f
Lê Minh <leminhson2398@outlook.com>
2025-05-15 21:28:46 +0700

    commit

--- src/routes/[[channel]]/settings/configs/+page.server.ts
+++ src/routes/[[channel]]/settings/configs/+page.server.ts
@@ -2,6 +2,7 @@ import { pageRequiresPermissions } from '$lib/api/client';
 import { PermissionEnum } from '$lib/gql/graphql';
 
 export const prerender = true;
+export const ssr = true;
 
 export const load = async (event) => {
   const user = await pageRequiresPermissions(event, PermissionEnum.ManageSettings);
