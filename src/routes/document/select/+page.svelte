fe6ce70c5eefc9135437a3c1eb6bad03e701b45a
nguyen_phuc <nguyenphuc02042004@gmail.com>
2025-05-21 15:51:59 +0700

    docs/update select document

--- src/routes/document/select/+page.svelte
+++ src/routes/document/select/+page.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
-	import { tranFunc } from '$i18n';
 	import { PRODUCT_LIST_QUERY_ADMIN } from '$lib/api/admin/product';
 	import { Button } from '$lib/components/ui';
 	import { Checkbox } from '$lib/components/ui/Input';
 	import { GraphqlPaginableSelect } from '$lib/components/ui/select';
 	import { Select } from '$lib/components/ui/select';
-	import SelectSkeleton from '$lib/components/ui/select/select-skeleton.svelte';
 	import { type QueryProductsArgs } from '$lib/gql/graphql';
 	import { toastStore } from '$lib/stores/ui/toast';
 
+	let generalVariant = $state<'info' | 'error' | 'warning' | 'success'>('info');
+	let graphqlVariant = $state<'info' | 'error' | 'warning' | 'success'>('info');
+
 	let value = $state<string[]>();
-	let isError = $state(false);
-	let isWarning = $state(false);
 	let disabled = $state(false);
 	let isMultiple = $state(false);
 
-	let isErrorGraphqlPaginableSelect = $state(false);
-	let isWarningGraphqlPaginableSelect = $state(false);
-	let loadingGraphqlPaginableSelect = $state(false);
 	let disabledGraphqlPaginableSelect = $state(false);
 	let isMultipleGraphqlPaginableSelect = $state(false);
 
@@ -29,8 +25,8 @@
 			label="GeneralSelect"
 			${isMultiple ? 'multiple' : ''}
 			${disabled ? 'disabled' : ''}
-			variant="${isError ? 'error' : isWarning ? 'warning' : 'info'}"
-			subText="${isError ? 'variant error' : isWarning ? 'variant warning' : ''}"
+			variant="${generalVariant}"
+			subText="${generalVariant}"
 			maxDisplay={1}
 			options={[
 				{ label: 'Option 1', value: '1' },
@@ -66,8 +62,8 @@
 			required
 			${isMultipleGraphqlPaginableSelect ? 'multiple' : ''}
 			${disabledGraphqlPaginableSelect ? 'disabled' : ''}
-			variant="${isErrorGraphqlPaginableSelect ? 'error' : isWarningGraphqlPaginableSelect ? 'warning' : 'info'}"
-			subText="${isErrorGraphqlPaginableSelect ? 'variant error' : isWarningGraphqlPaginableSelect ? 'variant warning' : ''}"
+			variant="${graphqlVariant}"
+			subText="${graphqlVariant}"
 			variableSearchQueryPath="filter.search"
 			bind:value
 		/>`;
@@ -86,68 +82,94 @@
 	<link rel="stylesheet" href="https://esm.sh/@wooorm/starry-night@3/style/both" />
 </svelte:head>
 
-<div class="flex flex-col gap-3">
-	<!-- General Select -->
-	<div class="bg-white rounded-md border border-gray-200 p-2">
-		<h1 class="p-2">General Select</h1>
-		<Checkbox bind:checked={isMultiple} label="Multiple" class="mb-2" />
-		<Checkbox bind:checked={isError} label="Error" class="mb-2" />
-		<Checkbox bind:checked={isWarning} label="Warning" class="mb-2" />
-		<Checkbox bind:checked={disabled} label="Disabled" class="mb-2" />
-		<Select
-			size="sm"
-			label="GeneralSelect"
-			multiple={isMultiple}
-			variant={isError ? 'error' : isWarning ? 'warning' : 'info'}
-			subText={isError ? 'variant error' : isWarning ? 'variant warning' : undefined}
-			{disabled}
-			onchange={console.log}
-			maxDisplay={1}
-			options={[
-				{ label: 'Option 1', value: '1' },
-				{ label: 'Option 2', value: '2' },
-				{ label: 'Option 3', value: '3' },
-			]}
-			bind:value
-		/>
-		<Button class="mt-2" onclick={handleCopyGeneralSelect}>Copy GeneralSelect</Button>
+<div class="bg-white rounded-md border border-gray-200 p-2">
+	<h1 class="p-2">General Select</h1>
+
+	<Checkbox bind:checked={isMultiple} label="Multiple" class="mb-2" />
+	<Checkbox bind:checked={disabled} label="Disabled" class="mb-2" />
+
+	<div class="flex gap-4 flex-wrap mb-2">
+		<label>
+			<input type="radio" bind:group={generalVariant} value="info" />
+			<span class="ml-1">Info</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={generalVariant} value="success" />
+			<span class="ml-1">Success</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={generalVariant} value="warning" />
+			<span class="ml-1">Warning</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={generalVariant} value="error" />
+			<span class="ml-1">Error</span>
+		</label>
 	</div>
 
-	<!-- GraphQL Select -->
-	<div class="bg-white rounded-md border border-gray-200 p-2">
-		<h1 class="mb-2">GraphQL Select</h1>
-		<Checkbox bind:checked={isMultipleGraphqlPaginableSelect} label="Multiple" class="mb-2" />
-		<Checkbox bind:checked={isErrorGraphqlPaginableSelect} label="Error" class="mb-2" />
-		<Checkbox bind:checked={isWarningGraphqlPaginableSelect} label="Warning" class="mb-2" />
-		<Checkbox bind:checked={disabledGraphqlPaginableSelect} label="Disabled" class="mb-2" />
-		<GraphqlPaginableSelect
-			query={PRODUCT_LIST_QUERY_ADMIN}
-			variables={{
-				first: 20,
-				filter: {
-					search: '',
-				},
-			} as QueryProductsArgs}
-			resultKey="products"
-			optionValueKey="id"
-			optionLabelKey="name"
-			label="Product"
-			required
-			bind:value
-			variant={isErrorGraphqlPaginableSelect
-				? 'error'
-				: isWarningGraphqlPaginableSelect
-					? 'warning'
-					: 'info'}
-			subText={isErrorGraphqlPaginableSelect
-				? 'variant error'
-				: isWarningGraphqlPaginableSelect
-					? 'variant warning'
-					: undefined}
-			disabled={disabledGraphqlPaginableSelect}
-			multiple={isMultipleGraphqlPaginableSelect}
-			variableSearchQueryPath="filter.search"
-		/>
-		<Button class="mt-2" onclick={handleCopyGraphqlPaginableSelect}>Copy GraphqlPaginableSelect</Button>
+	<Select
+		size="sm"
+		label="GeneralSelect"
+		multiple={isMultiple}
+		variant={generalVariant}
+		subText={`variant ${generalVariant}`}
+		{disabled}
+		onchange={console.log}
+		maxDisplay={1}
+		options={[
+			{ label: 'Option 1', value: '1' },
+			{ label: 'Option 2', value: '2' },
+			{ label: 'Option 3', value: '3' },
+		]}
+		bind:value
+	/>
+	<Button class="mt-2" onclick={handleCopyGeneralSelect}>Copy GeneralSelect</Button>
+</div>
+
+<div class="bg-white rounded-md border border-gray-200 p-2 mt-5">
+	<h1 class="mb-2">GraphQL Select</h1>
+
+	<Checkbox bind:checked={isMultipleGraphqlPaginableSelect} label="Multiple" class="mb-2" />
+	<Checkbox bind:checked={disabledGraphqlPaginableSelect} label="Disabled" class="mb-2" />
+
+	<div class="flex gap-4 flex-wrap mb-2">
+		<label>
+			<input type="radio" bind:group={graphqlVariant} value="info" />
+			<span class="ml-1">Info</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={graphqlVariant} value="success" />
+			<span class="ml-1">Success</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={graphqlVariant} value="warning" />
+			<span class="ml-1">Warning</span>
+		</label>
+		<label>
+			<input type="radio" bind:group={graphqlVariant} value="error" />
+			<span class="ml-1">Error</span>
+		</label>
 	</div>
+
+	<GraphqlPaginableSelect
+		query={PRODUCT_LIST_QUERY_ADMIN}
+		variables={{
+			first: 20,
+			filter: { search: '' },
+		} as QueryProductsArgs}
+		resultKey="products"
+		optionValueKey="id"
+		optionLabelKey="name"
+		label="Product"
+		required
+		bind:value
+		variant={graphqlVariant}
+		subText={`variant ${graphqlVariant}`}
+		disabled={disabledGraphqlPaginableSelect}
+		multiple={isMultipleGraphqlPaginableSelect}
+		variableSearchQueryPath="filter.search"
+	/>
+	<Button class="mt-2" onclick={handleCopyGraphqlPaginableSelect}
+		>Copy GraphqlPaginableSelect</Button
+	>
 </div>
