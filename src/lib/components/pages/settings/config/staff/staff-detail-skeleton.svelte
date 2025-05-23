67d423df59106a905db8c8c7fbac74040fb1f35a
nguyen_phuc <nguyenphuc02042004@gmail.com>
2025-05-23 15:46:06 +0700

    fix: staff detail skeleton

--- src/lib/components/pages/settings/config/staff/staff-detail-skeleton.svelte
+++ src/lib/components/pages/settings/config/staff/staff-detail-skeleton.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
+	import SelectSkeleton from '$lib/components/ui/select/select-skeleton.svelte';
 	import { Skeleton, SkeletonContainer } from '$lib/components/ui/Skeleton';
 </script>
 
-<div class="flex gap-2 flex-col h-full bg-white border border-gray-200 rounded-lg">
-	<SkeletonContainer class="p-4">
+<div class="flex gap-4 flex-col h-full bg-white border border-gray-200 rounded-lg p-4">
+	<SkeletonContainer>
 		<Skeleton class="h-18 w-18 rounded-full" />
 	</SkeletonContainer>
 	<div class="flex flex-row">
-		<SkeletonContainer class="w-1/2">
+		<SkeletonContainer>
 			<Skeleton class="h-4 w-16 mb-2"/>
-			<Skeleton class="h-8"/>
+			<SelectSkeleton/>
 		</SkeletonContainer>
-		<SkeletonContainer class="w-1/2">
+		<SkeletonContainer>
 			<Skeleton class="h-4 w-16 mb-2"/>
-			<Skeleton class="h-8"/>
+			<SelectSkeleton/>
 		</SkeletonContainer>
 	</div>
-	<SkeletonContainer class="p-4">
+	<SkeletonContainer>
 		<Skeleton class="h-4 w-16 mb-2"/>
-		<Skeleton class="h-8"/>
+		<SelectSkeleton/>
 	</SkeletonContainer>
-	<div class="flex p-4">
+	<div class="flex">
 		<Skeleton class="h-4 w-4 mr-2"/>
 		<Skeleton class="h-4 w-20"/>
 	</div>
