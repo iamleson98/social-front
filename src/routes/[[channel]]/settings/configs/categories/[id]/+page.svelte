3bf0e58d640d8600ce46a058518da0b216ab4822
nguyen_phuc <nguyenphuc02042004@gmail.com>
2025-06-19 15:08:43 +0700

    feat: update detail category

--- src/routes/[[channel]]/settings/configs/categories/[id]/+page.svelte
+++ src/routes/[[channel]]/settings/configs/categories/[id]/+page.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import { CATEGORY_DETAIL_QUERY } from '$lib/api';
-	import { CATEGORY_DELETE_MUTATION } from '$lib/api/admin/category';
+	import { CATEGORY_DELETE_MUTATION, CATEGORY_UPDATE_MUTATION } from '$lib/api/admin/category';
 	import { GRAPHQL_CLIENT } from '$lib/api/client';
 	import { operationStore } from '$lib/api/operation';
 	import ActionBar from '$lib/components/pages/settings/common/action-bar.svelte';
@@ -20,8 +20,12 @@
 	} from '$lib/gql/graphql';
 	import { ALERT_MODAL_STORE } from '$lib/stores/ui/alert-modal';
 	import { AppRoute } from '$lib/utils';
+	import type { MediaObject } from '$lib/utils/types';
 	import { checkIfGraphqlResultHasError } from '$lib/utils/utils';
 	import { onMount } from 'svelte';
+
+	let media = $state<MediaObject[]>([]);
+	let generalFormOk = $state(false);
 
 	const categoryQuery = operationStore<Pick<Query, 'category'>, QueryCategoryArgs>({
 		kind: 'query',
@@ -29,6 +33,7 @@
 		variables: {
 			id: page.params.id,
 		},
+		requestPolicy: 'cache-and-network',
 		pause: !page.params.id,
 	});
 
@@ -59,6 +64,14 @@
 						description: seoDescription,
 					},
 				};
+				if (backgroundImage)
+					media = [
+						{
+							alt: backgroundImage.alt || '',
+							url: backgroundImage.url,
+							type: 'image',
+						},
+					];
 			}
 		}),
 	);
@@ -82,7 +95,30 @@
 		});
 	};
 
-	const handleUpdate = async () => {};
+	const handleUpdate = async () => {
+		loading = true;
+		performUpdateMetadata = true;
+
+		const result = await GRAPHQL_CLIENT.mutation(CATEGORY_UPDATE_MUTATION, {
+			id: page.params.id,
+			input: {
+				...categoryInput,
+				description: JSON.stringify(categoryInput.description),
+				backgroundImage: media[0]?.file,
+				backgroundImageAlt: media[0]?.alt,
+			},
+		});
+
+		loading = false;
+
+		if (checkIfGraphqlResultHasError(result, 'categoryUpdate', 'Category updated successfully'))
+			return;
+
+		categoryQuery.reexecute({
+			variables: { id: page.params.id },
+			context: { requestPolicy: 'network-only' },
+		});
+	};
 </script>
 
 {#if $categoryQuery.fetching}
@@ -98,8 +134,10 @@
 				bind:description={categoryInput.description}
 				bind:slug={categoryInput.slug!}
 				bind:seoTitle={categoryInput.seo!.title!}
+				bind:media
 				bind:seoDescription={categoryInput.seo!.description!}
 				{loading}
+				bind:ok={generalFormOk}
 			/>
 
 			<GeneralMetadataEditor
