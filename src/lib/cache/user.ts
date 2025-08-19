e205288d2646b2b3df9184b8efdab156b6d436bc
Son Le <leminhson2398@outlook.com>
2025-08-19 16:01:15 +0700

    cm

--- src/lib/cache/user.ts
+++ src/lib/cache/user.ts
@@ -1,20 +1,20 @@
 import type { User } from "$lib/gql/graphql";
 import { Cache } from "./ttl-cache"
 import { decode, encode } from '@msgpack/msgpack';
-import { decodeJWT } from "$lib/utils/jwt";
+// import { decodeJWT } from "$lib/utils/jwt";
 
 
 export const getUserByJWT = async (jwtToken: string) => {
-  const jwtObject = decodeJWT(jwtToken);
+  // const jwtObject = decodeJWT(jwtToken);
 
-  if (Cache.has(jwtObject.user_id))
-    return decode(Cache.get(jwtObject.user_id))
+  if (Cache.has(jwtToken))
+    return decode(Cache.get(jwtToken))
 
   return null;
 };
 
 export const setJwtWithUser = async (jwtToken: string, user: User) => {
-  const jwtObject = decodeJWT(jwtToken);
+  // const jwtObject = decodeJWT(jwtToken);
 
-  return Cache.set(jwtObject.user_id, encode(user));
+  return Cache.set(jwtToken, encode(user));
 };
