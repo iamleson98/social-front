6aedcc0314045491d4c4d3b03e0d0d1184398d3a
iamleson98 <leminhson2398@outlook.com>
2025-11-01 15:49:45 +0700

    cm

--- src/lib/components/ui/Table-v2/Table.svelte
+++ src/lib/components/ui/Table-v2/Table.svelte
@@ -1,10 +1,33 @@
 <script lang="ts" generics="T extends Record<string, unknown>, K extends string">
+	import { ChevronLeft, ChevronRight } from '$lib/components/icons';
+	import { Button, IconButton } from '../Button';
+	import { DropDown, type MenuItemProps } from '../Dropdown';
+	import { type DropdownTriggerInterface } from '../Popover';
+	import { ROW_OPTIONS } from '../Table/types';
+	import type { TableProps } from './types';
 	import { createSvelteTable, FlexRender } from './ui/data-table';
 	import { Body, Cell, Head, Header, Root, Row } from './ui/table';
-	import type { TableProps } from './types';
-	import { getCoreRowModel } from '@tanstack/table-core';
+	import {
+		getCoreRowModel,
+		getPaginationRowModel,
+		getSortedRowModel,
+		type PaginationState,
+		type RowSelectionState,
+		type SortingState,
+	} from '@tanstack/table-core';
 
-	let { columns, items, className }: TableProps<T, K> = $props();
+	let {
+		columns,
+		items,
+		className,
+		rowsPerPage = 100,
+		onChangeRowsPerPage,
+		disabled,
+	}: TableProps<T, K> = $props();
+
+	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: rowsPerPage });
+	let sorting = $state<SortingState>([]);
+	let rowSelection = $state<RowSelectionState>({});
 
 	const Table = createSvelteTable({
 		get data() {
@@ -12,11 +35,52 @@
 		},
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		state: {
+			get pagination() {
+				return pagination;
+			},
+			get sorting() {
+				return sorting;
+			},
+			get rowSelection() {
+				return rowSelection;
+			},
+		},
+		onSortingChange: (updater) => {
+			if (typeof updater === 'function') {
+				sorting = updater(sorting);
+			} else {
+				sorting = updater;
+			}
+		},
+		onPaginationChange: (updater) => {
+			if (typeof updater === 'function') {
+				pagination = updater(pagination);
+			} else {
+				pagination = updater;
+			}
+		},
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		onRowSelectionChange: (updater) => {
+			if (typeof updater === 'function') {
+				rowSelection = updater(rowSelection);
+			} else {
+				rowSelection = updater;
+			}
+		},
 	});
+
+	// const handleRowsPerPageChange = (num: RowOptions) => {
+	// 	if (num !== innerRowsPerPage) {
+	// 		innerRowsPerPage = num;
+	// 		onChangeRowsPerPage?.(num);
+	// 	}
+	// };
 </script>
 
-<div class={[className, 'w-full overflow-x-auto']}>
-	<Root>
+<div class={[className, 'w-full bg-white rounded-lg border p-2 border-gray-200']}>
+	<Root class="table">
 		<Header>
 			{#each Table.getHeaderGroups() as headerGroup (headerGroup.id)}
 				<Row>
@@ -50,4 +114,37 @@
 			{/each}
 		</Body>
 	</Root>
+
+	<div class="flex items-center justify-end space-x-2 py-2 px-1">
+		<DropDown
+			placement="bottom-start"
+			options={ROW_OPTIONS.map<MenuItemProps>((num) => ({
+				children: `${num}`,
+				onclick: () => Table.setPageSize(num),
+			}))}
+		>
+			{#snippet trigger(opts: DropdownTriggerInterface)}
+				<Button size="xs" variant="light" {...opts} {disabled}>
+					{'Rows per page'}:
+					{pagination.pageSize}
+				</Button>
+			{/snippet}
+		</DropDown>
+		<IconButton
+			icon={ChevronLeft}
+			size="xs"
+			variant="light"
+			color="gray"
+			onclick={Table.previousPage}
+			disabled={!Table.getCanPreviousPage() || disabled}
+		/>
+		<IconButton
+			icon={ChevronRight}
+			size="xs"
+			variant="light"
+			color="gray"
+			onclick={Table.nextPage}
+			disabled={!Table.getCanNextPage() || disabled}
+		/>
+	</div>
 </div>
--- src/routes/lab/+page.svelte
+++ src/routes/lab/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-	import { Table } from '$lib/components/ui/Table-v2';
-	import type { ColumnDef } from '@tanstack/table-core';
+	import { ArrowDown, ArrowUp, Icon } from '$lib/components/icons';
+	import { IconButton } from '$lib/components/ui/Button';
+	import { Checkbox } from '$lib/components/ui/Input';
+	import { type RowOptions } from '$lib/components/ui/Table';
+	import { Table as TB } from '$lib/components/ui/Table-v2';
+	import { renderSnippet } from '$lib/components/ui/Table-v2/ui/data-table';
+	import type { ColumnDef, Row, Table, Column } from '@tanstack/table-core';
 
 	type Person = {
 		name: string;
 		age: number;
 		email: string;
 	};
+
+	const normal = '<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 3v18m-7-3l-3 3l-3-3m3 3V3m13 3l-3-3l-3 3"/>'
 
 	const items: Person[] = [
 		{
@@ -28,13 +35,22 @@
 
 	const columns: ColumnDef<Person>[] = [
 		{
+			id: 'select',
+			header: ({ table }) => renderSnippet(CheckAll, table),
+			cell: ({ row }) => renderSnippet(CheckSingle, row),
+		},
+		{
 			accessorKey: 'name',
 			header: 'Name',
 			// cell: ({ row }) => row.original.name,
+			enableSorting: true,
 		},
 		{
 			accessorKey: 'age',
-			header: 'Age',
+			header: ({ column }) => {
+				return renderSnippet(sort, column);
+			},
+			enableSorting: true,
 			// cell: ({ row }) => row.original.age,
 		},
 		{
@@ -45,4 +61,43 @@
 	];
 </script>
 
-<Table {items} {columns} />
+{#snippet sort(col: Column<Person, unknown>)}
+	{@const sortState = col.getIsSorted()}
+	<div class="flex items-center justify-between">
+		Age
+		<IconButton
+			size="xs"
+			color="gray"
+			variant="light"
+			onclick={col.getToggleSortingHandler()}
+		>
+			{#if !sortState}
+				<Icon icon={normal} size="xs" />
+			{:else if sortState === 'asc'}
+				<Icon icon={ArrowDown} size="xs" />
+			{:else if sortState === 'desc'}
+				<Icon icon={ArrowUp} size="xs" />
+			{/if}
+		</IconButton>
+	</div>
+{/snippet}
+
+{#snippet CheckAll(table: Table<Person>)}
+	<Checkbox
+		size="sm"
+		checked={table.getIsAllPageRowsSelected()}
+		aria-label="Select all"
+		onCheckChange={table.toggleAllPageRowsSelected}
+	/>
+{/snippet}
+
+{#snippet CheckSingle(row: Row<Person>)}
+	<Checkbox
+		size="sm"
+		aria-label="Select row"
+		checked={row.getIsSelected()}
+		onCheckChange={row.toggleSelected}
+	/>
+{/snippet}
+
+<TB {items} {columns} rowsPerPage={2 as RowOptions} />
